4003405e57f0f88aeb26573dda4c02d262b4af86
Mohammed Shamsi <mshamsi2@outlook.com>
2023-06-04 01:24:28 -0500

    more hiding color stuff

--- lua/nvim_web_devicons-config/init.lua
+++ lua/nvim_web_devicons-config/init.lua
@@ -1,14 +1,14 @@
-require'nvim-web-devicons'.setup {
- -- your personnal icons can go here (to override)
- -- you can specify color or cterm_color instead of specifying both of them
- -- DevIcon will be appended to `name`
- -- globally enable different highlight colors per icon (default to true)
- -- if set to false all icons will have the default icon's color
- color_icons = false;
+require 'nvim-web-devicons'.setup {
+    -- your personnal icons can go here (to override)
+    -- you can specify color or cterm_color instead of specifying both of them
+    -- DevIcon will be appended to `name`
+    -- globally enable different highlight colors per icon (default to true)
+    -- if set to false all icons will have the default icon's color
+    color_icons = false,
 
- -- globally enable default icons (default to false)
- -- will get overriden by `get_icons` option
- default = true;
+    -- globally enable default icons (default to false)
+    -- will get overriden by `get_icons` option
+    default = true,
 }
 
-require("nvim-web-devicons").set_default_icon('', '#654196')
+require("nvim-web-devicons").set_default_icon('', '#284547')
